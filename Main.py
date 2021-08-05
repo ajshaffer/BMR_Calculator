86462f8fd20f455db86f31a2a1e13c0d1faca4e9
AustinShaffer <ajshaffer05@hotmail.com>
2021-08-05 15:44:00 -0400

    Added ability to determine whether user input correlates to requested information.

--- Main.py
+++ Main.py
@@ -1,23 +1,54 @@
 import mFormula
 import wFormula
 
-weight = float(input("Enter weight in pounds: "))
+
+while True:
+    try:
+        weight = int(input("Enter weight in pounds: "))
+        if 1 <= weight <= 1000:
+            break
+        raise ValueError()
+    except ValueError:
+        print("Weight must be between 1 and 1000 pounds")
+
 weightKG = (float(weight) / 2.2)
 
-height = input("Enter height in inches: ")
+while True:
+    try:
+        height = int(input("Enter height in inches: "))
+        if 1 <= height <= 100:
+            break
+        raise ValueError()
+    except ValueError:
+        print("Height must be between 1 and 100 inches.")
+
 heightCM = (float(height) * 2.5)
 
-age = float(input("Enter age in years: "))
+while True:
+    try:
+        age = int(input("Enter age in years: "))
+        if 1 <= age <= 110:
+            break
+        raise ValueError()
+    except ValueError:
+        print("Age must be between 1 and 100 years old.")
 
-gender = input("Male or Female?: ")
+while True:
+    try:
+        gender = str(input("What is your gender?: ")).lower()
+        if gender == "Male".lower() or gender == "Female".lower():
+            break
+        raise ValueError()
+    except ValueError:
+        print("Gender must be Male or Female.")
 
 x = " Calories per day"
 
-if gender == "Male":
+if gender == "Male".lower():
     {
-        print(str((mFormula.num_1 * weightKG) + (mFormula.num_2 * heightCM) - (mFormula.num_3 * age) + mFormula.num_4) + str(x))
+        print(str((mFormula.num_1 * weightKG) + (mFormula.num_2 * heightCM) - (mFormula.num_3 * age) + mFormula.num_4) + x)
     }
-if gender == "Female":
+if gender == "Female".lower():
     {
-        print(str((wFormula.num_1 * weightKG) + (wFormula.num_2 * heightCM) - (wFormula.num_3 * age) + wFormula.num_4) + str(x))
+        print(str((wFormula.num_1 * weightKG) + (wFormula.num_2 * heightCM) - (wFormula.num_3 * age) + wFormula.num_4) + x)
     }
